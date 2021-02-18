20ca895eb69b50ddcc371977851842d6983d8ac7
theoboldalex <theoboldalex@gmail.com>
2021-02-18 20:03:06 +0000

    env var

--- weather.py
+++ weather.py
@@ -1,11 +1,12 @@
 import requests
 import json
+import os
 
 def main():
     print("Welcome to the weather app.")
-    user_location = input('Which city would you like the current weather for?\n')
+    user_location = input("Which city would you like the current weather for?\n")
 
-    res = requests.get(f'https://api.openweathermap.org/data/2.5/weather?q={user_location}&units=metric&appid=59abeaf902cbe89fece94e4fbcb1074f')
+    res = requests.get(f"https://api.openweathermap.org/data/2.5/weather?q={user_location}&units=metric&appid={os.environ['OPEN_WEATHER_API_KEY']}")
 
     data = res.json()
     weather_report(data)
